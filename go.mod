be7952cc6debfa07746eb9beaac1f7b0da0aa4ff
Christopher Ganga <christopher.ganga@pvotal.tech>
2023-08-07 16:06:22 +0200

    feat(PVO-311): add specifier parsing

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ specifier.go
@@ -0,0 +1,119 @@
+package uof
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/dustin/go-humanize"
+)
+
+func ParseSpecifier(name string, specifiers map[string]string, players map[int]Player, fixture Fixture) (string, error) {
+	for key, val := range specifiers {
+		switch {
+		case strings.Contains(name, "{"+key+"}"):
+			name = strings.ReplaceAll(name, "{"+key+"}", val)
+		case strings.Contains(name, "{!"+key+"}"):
+			intVal, err := strconv.Atoi(val)
+			if err != nil {
+				return "", fmt.Errorf("invalid number in specifier with ordinal operator: %s", val)
+			}
+			name = strings.ReplaceAll(name, "{!"+key+"}", humanize.Ordinal(intVal))
+		case strings.Contains(name, "{"+key+"-"):
+			i := strings.Index(name, "{"+key+"-")
+			j := strings.Index(name[i:], "}") + i
+			nStr := name[i+len(key)+2 : j]
+			n, err := strconv.Atoi(nStr)
+			if err != nil {
+				return "", fmt.Errorf("invalid number in name with sub(-) specifier: %s", name)
+			}
+			intVal, err := strconv.Atoi(val)
+			if err != nil {
+				return "", fmt.Errorf("invalid number in specifier with sub(-) operator: %s", val)
+			}
+			result := intVal - n
+			name = name[:i] + strconv.Itoa(result) + name[j+1:]
+		case strings.Contains(name, "{"+key+"+"):
+			i := strings.Index(name, "{"+key+"+")
+			j := strings.Index(name[i:], "}") + i
+			nStr := name[i+len(key)+2 : j]
+			n, err := strconv.Atoi(nStr)
+			if err != nil {
+				return "", fmt.Errorf("invalid number in name with sum(+) specifier: %s", name)
+			}
+			intVal, err := strconv.Atoi(val)
+			if err != nil {
+				return "", fmt.Errorf("invalid number in specifier with sum(+) operator: %s", val)
+			}
+			result := intVal + n
+			name = name[:i] + strconv.Itoa(result) + name[j+1:]
+		case strings.Contains(name, "{!"+key+"-"):
+			i := strings.Index(name, "{!"+key+"-")
+			j := strings.Index(name[i:], "}") + i
+			nStr := name[i+len(key)+3 : j]
+			n, err := strconv.Atoi(nStr)
+			if err != nil {
+				return "", fmt.Errorf("invalid number in name with sub(-) ordinal specifier: %s", name)
+			}
+			intVal, err := strconv.Atoi(val)
+			if err != nil {
+				return "", fmt.Errorf("invalid number in specifier with sub(-) ordinal operator: %s", val)
+			}
+			result := intVal - n
+			name = name[:i] + humanize.Ordinal(result) + name[j+1:]
+		case strings.Contains(name, "{!"+key+"+"):
+			i := strings.Index(name, "{!"+key+"+")
+			j := strings.Index(name[i:], "}") + i
+			nStr := name[i+len(key)+3 : j]
+			n, err := strconv.Atoi(nStr)
+			if err != nil {
+				return "", fmt.Errorf("invalid number in name with sum(+) ordinal specifier: %s", name)
+			}
+			intVal, err := strconv.Atoi(val)
+			if err != nil {
+				return "", fmt.Errorf("invalid number in specifier with sum(+) ordinal operator: %s", val)
+			}
+			result := intVal + n
+			name = name[:i] + humanize.Ordinal(result) + name[j+1:]
+		case strings.Contains(name, "{+"+key+"}"):
+			i := strings.Index(name, "{")
+			j := strings.Index(name[i:], "}") + i
+			intVal, err := strconv.Atoi(val)
+			if err != nil {
+				return "", fmt.Errorf("invalid number in specifier with signed operator: %s", val)
+			}
+			name = name[:i] + "+" + strconv.Itoa(intVal) + name[j+1:]
+		case strings.Contains(name, "{-"+key+"}"):
+			i := strings.Index(name, "{")
+			j := strings.Index(name[i:], "}") + i
+			intVal, err := strconv.Atoi(val)
+			if err != nil {
+				return "", fmt.Errorf("invalid number in specifier with signed operator: %s", val)
+			}
+			name = name[:i] + "-" + strconv.Itoa(intVal) + name[j+1:]
+		case strings.Contains(name, "{%player}"):
+			playerID := URN(val).ID()
+			player, ok := players[playerID]
+			if !ok {
+				return "", fmt.Errorf("player with id %d not found", playerID)
+			}
+			name = strings.ReplaceAll(name, "{%player}", player.FullName)
+		case strings.Contains(name, "{$event}"):
+			name = strings.ReplaceAll(name, "{$event}", fixture.Name)
+		case strings.Contains(name, "{$competitor"):
+			i := strings.Index(name, "{$competitor")
+			j := strings.Index(name[i:], "}") + i
+			index := name[i+len("{$competitor") : j]
+			indexInt, err := strconv.Atoi(index)
+			if err != nil {
+				return "", fmt.Errorf("invalid number in specifier with competitor operator: %s", index)
+			}
+			if indexInt > len(fixture.Competitors) {
+				return "", fmt.Errorf("invalid number in specifier with competitor operator: %s", index)
+			}
+			competitor := fixture.Competitors[indexInt-1]
+			name = name[:i] + competitor.Name + name[j+1:]
+		}
+	}
+	return name, nil
+}
